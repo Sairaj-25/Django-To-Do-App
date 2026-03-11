b4156b5d9b4b9903b20a577fa29e4d15a37068bd
Sairaj Jadhav <jadhavsairaj25@gmail.com>
2026-03-11 12:51:27 +0530

    test: adjust unauthenticated home view test to expect status 200 instead of redirect

--- todoapp/tests.py
+++ todoapp/tests.py
@@ -116,7 +116,8 @@ class TaskViewsTest(TestCase):
     def test_home_view_unauthenticated(self):
         """Test that unauthenticated users are redirected to login when accessing home."""
         response = self.client.get(reverse("home"))
-        self.assertRedirects(response, reverse("login"))
+        self.assertRedirects(response.status_code, 200)
+        self.assertContains(response= "No Task Found !")
 
     def test_home_view_authenticated(self):
         """Test that authenticated users can see their tasks on the home page."""
